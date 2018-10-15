3ae7f08cc5d5c4b3fccf1bdd920ee8c1e3c23a0a
alinex <info@alinex.de>
2018-10-15 15:53:35 +0200

    fix daily pattern read

--- include/log.bash
+++ include/log.bash
@@ -144,8 +144,8 @@ log () {
     # rotate log files
     if [ -n "$LOG_FILE" ] && [ "$LOG_FILE" != "STDERR" ] && [ -e "$LOG_FILE" ]; then
         if [ -n "$LOG_ROTATE_TIME" ]; then
-            local file_date=$(date -d $(stat -c %y "$LOG_FILE" ) $_log_rotate_time[$LOG_ROTATE_TIME])
-            local today=$(date $_log_rotate_time[$LOG_ROTATE_TIME])
+            local file_date=$(date -d $(stat -c %y "$LOG_FILE" ) "$_log_rotate_time[$LOG_ROTATE_TIME]")
+            local today=$(date "$_log_rotate_time[$LOG_ROTATE_TIME]")
             if [ "$file_date" != "$today" ]; then
                 mv "$LOG_FILE" "$LOG_FILE.$file_date"
                 [ -n "$LOG_ROTATE_COMPRESS" ] && gzip -q --best "$LOG_FILE.$file_date"
