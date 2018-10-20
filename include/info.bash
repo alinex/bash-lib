49a0177aac306a4dbe33f0ea36bfb0f75031ae06
alinex <info@alinex.de>
2018-10-20 16:51:39 +0200

    add info package

--- include/info.bash
+++ include/info.bash
@@ -12,28 +12,50 @@
 source_dir=$(dirname "${BASH_SOURCE[0]}")
 source "$source_dir/log.bash" # load log handler
 
-# output debian type like stretch/sid
-info_debian() {
-  file=/etc/debian_version
-  [ -f $file ] || return 1
-  case $(cat $file) in
-  7*|wheezy*) echo 7;;
-  8*|jessie*) echo 8;;
-  9*|stretch*) echo 9;;
-  1ß*|buster*) echo 10;;
-  11*|bullseye*) echo 11;;
-  12*|bookworm*) echo 12;;
-  *) cat $file;;
+os_type() {
+  if [ -f /etc/debian_version ]; then
+    echo debian
+  fi
+}
+
+# output debian major number
+os() {
+  type=$(os_type)
+  case $type in
+  debian)
+    file=/etc/debian_version
+    [ -f $file ] || return 1
+    case $(cat $file) in
+    7*|wheezy*) echo 7;;
+    8*|jessie*) echo 8;;
+    9*|stretch*) echo 9;;
+    1ß*|buster*) echo 10;;
+    11*|bullseye*) echo 11;;
+    12*|bookworm*) echo 12;;
+    *) cat $file;;
+    esac
+    ;;
+  *)
+    log_exit ALERT "unknown operating system $type not supported"
+    ;;
   esac
 }
 
 # usage: info_package <name>
 # output: version number
-info_package() {
+package() {
   [ "$#" -ne 1 ] && log_exit ALERT "parameter missing call: info_package <name>"
 
-  data=$(dpkg -s $1 2>/dev/null)
-  [ $? -eq 0 ] || return 1
-  echo $data | grep Version | sed "s/Version: //"
-  return 0
+  type=$(os_type)
+  case $type in
+  debian)
+    data=$(dpkg -s $1 2>/dev/null)
+    [ $? -eq 0 ] || return 1
+    echo $data | grep Version | sed "s/Version: //"
+    return 0
+    ;;
+  *)
+    log_exit ALERT "unknown operating system $type not supported"
+    ;;
+  esac
 }
