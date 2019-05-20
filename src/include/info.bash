87d747627897641514732d197aab899bc176451d
alinex <info@alinex.de>
2019-05-20 15:38:01 +0200

    add mq detection

--- src/include/info.bash
+++ src/include/info.bash
@@ -155,11 +155,13 @@ ip_info() {
 declare -A _package
 _package[apache]="apache2"
 _package[tomcat]="tomcat8 tomcat7 tomcat6"
-_package[jdk]="openjdk-11-jdk openjdk-10-jdk openjdk-9-jdk openjdk-8-jdk openjdk-7-jdk openjdk-6-jdk"
+_package[jdk]="openjdk-11-jdk openjdk-10-jdk openjdk-9-jdk openjdk-8-jdk openjdk-7-jdk openjdk-6-jdk oracle-java8-installer"
 _package[jre]="openjdk-11-jre openjdk-10-jre openjdk-9-jre openjdk-8-jre openjdk-7-jre openjdk-6-jre"
 _package[postgresql]="postgresql-11 postgresql-10 postgresql-9.6 postgresql-9.4 postgresql-9.3"
 _package[gitlab]="gitlab-ee gitlab-ce"
 _package[subversion]="subversion"
+_package[activemq]="activemq"
+_package[rabbitmq]="rabbitmq-server"
 declare -r _package
 
 # Usage: package <name>
