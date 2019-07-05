2353ca742d7201124ab5477d5927a6cd3df629b3
alinex <info@alinex.de>
2019-07-05 12:20:08 +0200

    fix bug in needsudo with command arguments

--- src/include/info.bash
+++ src/include/info.bash
@@ -217,9 +217,10 @@ package_list() {
 # output: <group> <middleware> <setting> <value>
 #         tomcat     tomcat8_1   uri http://:8080
 middleware() {
+    usesudo=$(usesudo)
     # tomcat
     for path in $(echo /var/lib/tomcat*); do
-        port=$(cat $path/conf/server.xml | sed 's/<!--/\x0<!--/g;s/-->/-->\x0/g' | grep -zv '^<!--' | tr -d '\0' | grep 'protocol="HTTP' | sed 's/^.*port="//;s/".*//')
+        port=$($usesudo cat $path/conf/server.xml | sed 's/<!--/\x0<!--/g;s/-->/-->\x0/g' | grep -zv '^<!--' | tr -d '\0' | grep 'protocol="HTTP' | sed 's/^.*port="//;s/".*//')
         echo tomcat $(basename $path) uri http://$(ip_main):$port
     done
 }
@@ -228,9 +229,10 @@ middleware() {
 #         tomcat        tomcat8_1 xxx  uri http://:8080/context
 #         tomcat        tomcat8_1 xxx  threads 500
 app() {
+    usesudo=$(usesudo)
     # tomcat
     for path in $(ls -d /var/lib/tomcat* 2>/dev/null); do
-        port=$(cat $path/conf/server.xml | sed 's/<!--/\x0<!--/g;s/-->/-->\x0/g' | grep -zv '^<!--' | tr -d '\0' | grep 'protocol="HTTP' | sed 's/^.*port="//;s/".*//')
+        port=$($usesudo cat $path/conf/server.xml | sed 's/<!--/\x0<!--/g;s/-->/-->\x0/g' | grep -zv '^<!--' | tr -d '\0' | grep 'protocol="HTTP' | sed 's/^.*port="//;s/".*//')
         for webapp in $(ls $path/webapps/ | egrep -v 'ROOT|.war'); do
             echo tomcat $(basename $path) $webapp uri http://$(ip_main):$port/$webapp
         done
