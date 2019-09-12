8aa75550ca4f044ccd4555c9c01c6dffe1df1e3f
alinex <info@alinex.de>
2019-09-12 15:28:04 +0200

    update package detection

--- src/include/info.bash
+++ src/include/info.bash
@@ -157,7 +157,7 @@ ip_info() {
 
 # lookup for real names
 declare -A _package
-_package[apache]="apache2 nginx"
+_package[apache]="apache2"
 _package[nginx]="nginx"
 _package[tomcat]="tomcat9 tomcat8 tomcat7 tomcat6"
 _package[jdk]="openjdk-11-jdk openjdk-10-jdk openjdk-9-jdk openjdk-8-jdk openjdk-7-jdk zulu-7 openjdk-6-jdk oracle-java8-installer"
